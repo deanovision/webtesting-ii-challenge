4a6d22371603eabd6dfd7000d8ea6a56f187db48
deanovision <rodeanfraser@gmail.com>
2019-05-14 17:57:18 -0400

    added a test for strikes and balls on display component

--- app/src/Display.js
+++ app/src/Display.js
@@ -1,11 +1,25 @@
 import React from "react";
 
 const Display = ({ info }) => {
+  const strikes = () => {
+    if (!info) {
+      return "Strikes: 0";
+    } else {
+      return `Strikes: ${info.strikes}`;
+    }
+  };
+  const balls = () => {
+    if (!info) {
+      return "Balls: 0";
+    } else {
+      return `Balls: ${info.balls}`;
+    }
+  };
   return (
     <div>
       <h2>Play Ball!</h2>
-      <span>{`Strikes: ${info.strikes} `}</span>
-      <span>{`Balls: ${info.balls}`} </span>
+      <span>{strikes()} </span>
+      <span>{balls()} </span>
     </div>
   );
 };
